096e9816b28c602e3c6916045e7db1f366267ab0
michele byman <michelebyman@gmail.com>
2019-02-11 16:30:19 +0100

    added navbar and background plus css for nav plus javascript for toggle-navbar

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
-
+// NAV - toggle(function() {
+$('#nav-toggle, #shadow').click(function() {
+  $(".menu, #shadow").animate({
+    opacity: 1,
+    width: "toggle"
+  }, 300, function() {
+    // Animation complete.
+  });
+  //$(".btn-primary, .jumbotron").css("opacity", "0.7");
+});
