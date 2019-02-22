f34d4ccfe9c10321d3526147033ff307077a650b
michele byman <michelebyman@gmail.com>
2019-02-22 12:40:55 +0100

    nothing done, just for fun me and seb

--- javascript.js
+++ javascript.js
@@ -1,10 +1,11 @@
 // NAV - toggle(function() {
 $('#nav-toggle, #shadow').click(function() {
-  $(".menu, #shadow").animate({
+  $(".menu").animate({
     opacity: 1,
     width: "toggle"
-  }, 300, function() {
+  }, 200, function() {
     // Animation complete.
   });
-  //$(".btn-primary, .jumbotron").css("opacity", "0.7");
+  $(".info-links").toggle(201);
+  $("#shadow").toggle();
 });
